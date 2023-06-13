ff0f6170d9030573e7106d3613eb288fa4068cca
aaymecho <aaymecho3@gatech.edu>
2023-06-13 00:58:04 -0400

    added rgb/buttons

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# Etch-a-Sketch
+
+This codespace contains my implementation of the Etch-a-Sketch project, which is part of The Odin Project curriculum. The project is a simple web-based drawing application inspired by the classic Etch-a-Sketch toy.
+
+## Description
+
+The Etch-a-Sketch project is designed to test my understanding of HTML, CSS, and JavaScript. The web application allows users to draw on a grid by hovering their mouse over the cells. The user can also adjust the size of the grid and clear the drawing.
+
+The main objectives of the project are:
+
+- Creating a grid of cells using HTML and CSS
+- Implementing JavaScript event listeners to respond to user actions
+- Manipulating the CSS styling of the cells to create the drawing effect
+- Allowing users to adjust the size of the grid and clear the drawing
+
+## Project Structure
+
+The project consists of the following files:
+
+- `index.html`: The HTML structure of the application, including the grid and buttons.
+- `style.css`: The CSS file that defines the appearance and layout of the application.
+- `main.js`: The JavaScript file that contains the logic for drawing on the grid and handling user interactions.
--- main.js
+++ main.js
@@ -1,16 +1,45 @@
+let size = 16;
+
 function generatePixel(size) {
   const container = document.querySelector(".container");
-
   for (let column = 0; column < size; column++) {
     const div_container = document.createElement("div");
     div_container.classList.add("div_container");
     container.appendChild(div_container);
     for (let row = 0; row < size; row++) {
       const pixel = document.createElement("div");
-      pixel.classList.add("pixel");
+      pixel.classList.add(`pixel`);
+      pixel.addEventListener('mouseover', () => {
+        pixel.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+      }, { once: true });
       div_container.appendChild(pixel);
     }
+
   }
 }
 
-generatePixel(10);
+
+function reset() {
+  const container = document.querySelector(".container");
+  container.replaceChildren();
+  generatePixel(size);
+
+}
+//reset button
+const reset_button = document.querySelector(".reset");
+reset_button.addEventListener("click", () => {
+  reset();
+})
+
+
+//resize button
+const resize = document.querySelector(".resize");
+resize.addEventListener("click", () => {
+  size = prompt("Desired value?");
+  reset();
+})
+
+
+
+
+generatePixel(size);
